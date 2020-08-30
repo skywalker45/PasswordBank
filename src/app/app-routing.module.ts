b93b2a3781139fc21e49c18190bd75b3332d4742
skywalker45 <daniel_salami@outlook.com>
2020-08-29 20:20:04 -0700

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { AddPassComponent } from './add-pass/add-pass.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetPassComponent } from './get-pass/get-pass.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'addPassword',
+    component: AddPassComponent
+  },
+  {
+    path: 'getPassword',
+    component: GetPassComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dist/app-routing.module.js
@@ -0,0 +1,44 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppRoutingModule = void 0;
+var add_pass_component_1 = require("./add-pass/add-pass.component");
+var home_component_1 = require("./home/home.component");
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var get_pass_component_1 = require("./get-pass/get-pass.component");
+var routes = [
+    {
+        path: '',
+        component: home_component_1.HomeComponent
+    },
+    {
+        path: 'addPassword',
+        component: add_pass_component_1.AddPassComponent
+    },
+    {
+        path: 'getPassword',
+        component: get_pass_component_1.GetPassComponent
+    },
+    {
+        path: '**',
+        component: home_component_1.HomeComponent
+    }
+];
+var AppRoutingModule = /** @class */ (function () {
+    function AppRoutingModule() {
+    }
+    AppRoutingModule = __decorate([
+        core_1.NgModule({
+            imports: [router_1.RouterModule.forRoot(routes)],
+            exports: [router_1.RouterModule]
+        })
+    ], AppRoutingModule);
+    return AppRoutingModule;
+}());
+exports.AppRoutingModule = AppRoutingModule;
